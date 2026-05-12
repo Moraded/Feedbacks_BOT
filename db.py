7154d93cf8377248012ba8d340ecaf010ff0708f
Morad <zakirovm451@gmail.com>
2026-05-12 16:45:53 +0500

    убрал лишние соединения, теперь только одно через wb_bot.py диспетчер aiogram, использовал возможности row_factory

--- db.py
+++ db.py
@@ -7,20 +7,19 @@ logger = logging.getLogger(__name__)
 class DatabaseBot:
   def __init__(self, db_path: str):
     self.db_path = db_path
-    self.db = None
+    self.db: aiosqlite.Connection | None = None
 
-  async def connect(self):
+  async def connect(self) -> None:
     self.db = await aiosqlite.connect(self.db_path)
     self.db.row_factory = aiosqlite.Row
     await self._create_tables()
 
   async def get_all_users(self) -> list[int]:
-    async with aiosqlite.connect(self.db_path) as db:
-      cursor = await db.execute("SELECT DISTINCT user_id FROM users")
+    async with self.db.execute("SELECT DISTINCT user_id FROM users") as cursor:
       rows = await cursor.fetchall()
-      return [row[0] for row in rows]
+      return [row["user_id"] for row in rows]
 
-  async def _create_tables(self):
+  async def _create_tables(self) -> None:
     await self.db.execute("""
 CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY)""")
     await self.db.execute("""
@@ -33,10 +32,10 @@ CREATE TABLE IF NOT EXISTS cabinets (
                           is_active INTEGER DEFAULT 0,
                           UNIQUE(user_id, token))""")
     await self.db.commit()
-  async def close(self):
+  async def close(self) -> None:
     await self.db.close()
 
-  async def get_user_cabinets(self, user_id: int) -> str | None:
+  async def get_user_cabinets(self, user_id: int) -> list[aiosqlite.Row] | None:
     try:
       async with self.db.execute(
         "SELECT id, seller_name, brand_name, is_active FROM cabinets WHERE user_id = ?", (user_id,)
@@ -48,7 +47,7 @@ CREATE TABLE IF NOT EXISTS cabinets (
     return result
 
 
-  async def switch_cabinet(self, user_id, id):
+  async def switch_cabinet(self, user_id: int, id: int) -> bool:
     try:
       async with self.db.execute("UPDATE cabinets SET is_active = 0 WHERE user_id = ? AND is_active = 1", (user_id,)):
         async with self.db.execute("UPDATE cabinets SET is_active = 1 WHERE id = ? AND user_id = ?", (id, user_id)):
@@ -59,17 +58,17 @@ CREATE TABLE IF NOT EXISTS cabinets (
       return False
 
 
-  async def get_active_token(self, user_id):
+  async def get_active_token(self, user_id: int) -> str | None:
     try:
       async with self.db.execute("SELECT token FROM cabinets WHERE user_id = ? and is_active = 1", (user_id,)) as cursor:
         result = await cursor.fetchone()
-        token = result[0] if result else None
+        token = result["token"] if result else None
     except (aiosqlite.Error, TypeError):
       logger.exception("❌ Ошибка при получении токена из базы или новый пользователь")
       token = None
     return token
   
-  async def register_user(self, user_id):
+  async def register_user(self, user_id: int) -> bool:
     try:
       async with self.db.execute("INSERT OR IGNORE INTO users VALUES (?)", (user_id,)):
         await self.db.commit()
@@ -78,18 +77,18 @@ CREATE TABLE IF NOT EXISTS cabinets (
       logger.exception("❌ Ошибка при занесении пользователя в базу данных")
       return False
 
-  async def get_user_seller_name(self, token):
+  async def get_user_seller_name(self, token: str) -> str | None:
     try:
       async with self.db.execute("SELECT seller_name FROM cabinets WHERE token = ?", (token,)) as cursor:
         row = await cursor.fetchone()
-        seller_name = row[0] if row else None
+        seller_name = row["seller_name"] if row else None
     except (aiosqlite.Error, TypeError):
       logger.exception("Ошибка при получении имени продавца из базы")
       seller_name = None
     return seller_name
 
 
-  async def add_cabinet(self, user_id, token, seller_name, brand_name):
+  async def add_cabinet(self, user_id: int, token: str, seller_name: str, brand_name: str) -> bool | str:
     try:
       async with self.db.execute("UPDATE cabinets SET is_active = 0 WHERE user_id = ?", (user_id,)):
         async with self.db.execute("INSERT INTO cabinets (user_id, token, seller_name, brand_name, is_active) VALUES (?, ?, ?, ?, ?)", (user_id, token, seller_name, brand_name, 1)):
@@ -103,7 +102,7 @@ CREATE TABLE IF NOT EXISTS cabinets (
       return False
 
 
-  async def reset_token(self, user_id, token):
+  async def reset_token(self, user_id: int, token: str) -> bool:
     try:
       async with self.db.execute("DELETE FROM cabinets WHERE user_id = ? AND token = ?", (user_id, token)):
         await self.db.commit()
